e7249daec4325a6d7a8bfb6c5398e052775201e1
Matheus Musashi <126472450+MatheusMusashiTanaka@users.noreply.github.com>
2023-03-14 17:48:29 -0300

    Update and rename Algoritimo 3 sequencial to Algoritimo 3 sequencial.go

---
+++ Algoritimo 3 sequencial.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var peso float64
+	fmt.Println("Peso em kilos")
+	fmt.Scanln(&peso)
+	var altura float64
+	fmt.Println("Altura em metros:")
+	fmt.Scanln(&altura)
+
+	var IMC float64
+	IMC = peso / (altura * altura)
+	fmt.Println("IMC:", IMC)
+
+}
