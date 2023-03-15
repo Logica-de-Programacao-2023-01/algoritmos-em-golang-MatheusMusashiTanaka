a6d70b32af7b99c54957353e5421d816e418c7a6
Matheus Musashi <126472450+MatheusMusashiTanaka@users.noreply.github.com>
2023-03-15 13:28:24 -0300

    Update Algoritimo 5 sequencial.go

--- Algoritimo 5 sequencial.go
+++ Algoritimo 5 sequencial.go
@@ -1,1 +1,13 @@
+package main
 
+import "fmt"
+
+func main() {
+	var idade int
+	fmt.Println("idade em anos:")
+	fmt.Scanln(&idade)
+	var Idade_em_dias int
+	Idade_em_dias = idade * 365
+	fmt.Println("A Idade em dias é", Idade_em_dias)
+
+}
