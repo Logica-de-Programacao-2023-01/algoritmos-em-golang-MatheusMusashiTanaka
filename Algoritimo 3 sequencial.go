ec0f482e2b7f4f9133e0727eae107b8e3c3437f7
Matheus Musashi <126472450+MatheusMusashiTanaka@users.noreply.github.com>
2023-03-15 13:44:20 -0300

    Update Algoritimo 3 sequencial.go

--- Algoritimo 3 sequencial.go
+++ Algoritimo 3 sequencial.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	var peso float64
-	fmt.Println("Peso em kilos")
+	fmt.Println("Peso em quilos")
 	fmt.Scanln(&peso)
 	var altura float64
 	fmt.Println("Altura em metros:")
